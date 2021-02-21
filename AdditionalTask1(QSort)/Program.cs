6005aebb348360cad0cb1e15de620f95e94e17f3
Physichko <79219071+Physichko@users.noreply.github.com>
2021-02-21 15:03:32 +0300

    Добавил AdditionalTask1

---
+++ AdditionalTask1(QSort)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdditionalTask1_QSort_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
