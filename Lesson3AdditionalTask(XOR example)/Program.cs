442360919be00750b9b8f3c723a638a4090bf35d
Physichko <79219071+Physichko@users.noreply.github.com>
2021-02-23 23:16:33 +0300

    changed

--- Lesson3AdditionalTask(XOR example)/Program.cs
+++ Lesson3AdditionalTask(XOR example)/Program.cs
@@ -9,9 +9,9 @@ namespace Lesson3AdditionalTask_XOR_example_
             ushort secretKey = 0x0088; // Секретный ключ длиной 16 бит
             char character = 'A';
             character = (char)(character ^ secretKey);//Шифровка
-            Console.WriteLine(character);
+            Console.WriteLine("was A ->"+character);
             character = (char)(character ^ secretKey);//Расшифровка
-            Console.WriteLine(character);
+            Console.WriteLine("was E ->"+character);
             string STR = "ABRAKADABRA";
             char[] charr = STR.ToCharArray();
             for (int i = 0; i < charr.Length; i++)
