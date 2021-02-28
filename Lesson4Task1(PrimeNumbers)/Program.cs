39052a64e2e5293de7742caf23c0f179fdf9f63e
Physichko <79219071+Physichko@users.noreply.github.com>
2021-02-28 17:05:29 +0300

    Учёл замечания

--- Lesson4Task1(PrimeNumbers)/Program.cs
+++ Lesson4Task1(PrimeNumbers)/Program.cs
@@ -6,14 +6,17 @@ namespace Lesson4Task1_PrimeNumbers_
     {
         static bool IsPrime(int iter) //Метод проверяющий число на простоту
         {
+            bool isPrime = true;
             for (int i = 2; i <= iter/2; i++)
             {
                 if (iter % i == 0)
                 {
-                    return false;
+                    isPrime = false;
+                    break;
                 }
+                else continue;
             }
-            return true;
+            return isPrime;
         }
         static void Main()
         {
@@ -33,7 +36,7 @@ namespace Lesson4Task1_PrimeNumbers_
             {
                 for (int i = 1; i <= value; i++)
                 {
-                    Console.Write(IsPrime(i) ? new string($"{i} - простое число\n") : new string(""));
+                    Console.Write(IsPrime(i) ?$"{i} - простое число\n" :"");
                 }
             }
             else Console.WriteLine("Введено не число или число меньше 0");
@@ -57,45 +60,13 @@ namespace Lesson4Task1_PrimeNumbers_
                 while (value1 > i)
                 {
                     i++;
-                    Console.Write(IsPrime(i) ? new string($"{i} - простое число\n"):new string(""));
+                    Console.Write(IsPrime(i) ?$"{i} - простое число\n":"");
                 }
             }
             else Console.WriteLine("Введено не число или число меньше 0");
             //Закончилась часть программы через while
             #endregion
-            Console.WriteLine();
-            Console.WriteLine();
-            // Использование цикла for и замена for на while было представлено выше.
-            // Cейчас будет представлена работа с операторами break и continue
             
-            int a = 0;
-            while (a < 1000) 
-            {
-                a++;
-                Console.WriteLine("значение a увеличилось до {0}",a);
-                if (a < 15) 
-                {
-                    Console.WriteLine("a = {0} (благодаря continue (Должно быть должно быть увеличение до 15) )",a);
-                    continue;
-                }
-                    
-                for (int i = 0; i < 10; i++)
-                {
-                    a += 5;
-                    Console.WriteLine("Значение a благодаря увеличению а на значение 5 в цикле for вырасло до значения {0} (Пока а < 100 )", a);
-                    if (a > 100)
-                    {
-                        Console.WriteLine("a > 100 , значит при помощи break выходим из цикла for");
-                        break;
-                    }
-                }
-                Console.WriteLine("а вышло из цикла for повысив свое значение до {0}",a);
-                if (a > 100)
-                {
-                    Console.WriteLine($"{a} > 100, значит выходим из цикла while (хотя условие while : a < 1000)");
-                    break;
-                }
-            }
         }
     }
 }
