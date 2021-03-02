a38b5a6be34e42a423d779a3154e34b6dc010420
Physichko <79219071+Physichko@users.noreply.github.com>
2021-03-02 20:53:36 +0300

    upd

--- Lesson5Task1(Arrays)/Program.cs
+++ Lesson5Task1(Arrays)/Program.cs
@@ -7,20 +7,23 @@ namespace Lesson5Task1_Arrays_
         static void Main()
         {
             MethodsAndProperties methodsAndProperties = new MethodsAndProperties(new Random().Next(3, 10), new Random().Next(3, 10), new Random().Next(3, 10));
+            int[] intArray = new int[8];
+            int[,] twoDimentsionalArray = new int[methodsAndProperties.HeightForTwoDimensional, methodsAndProperties.WidthForTwoDimensional];
+            int[][] jaggedArray = new int[methodsAndProperties.AmountOfSubArrays][];
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(new string('-', 120));
             Console.WriteLine("Часть 1. Эта часть программы посвящена поиску последнего отрицательного значения в массиве : ");
             Console.WriteLine(new string('-', 120));
-            int[] intArray = new int[8];
             methodsAndProperties.ToFillArrayWithRandomNumbers(intArray);
             Console.WriteLine(methodsAndProperties.ShowArray(intArray));
             Console.WriteLine(methodsAndProperties.ToFindLastIndexOfNegativeNumberInArray(intArray));
             Console.WriteLine(new string('-', 120));
+
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(new string('-', 120));
             Console.WriteLine("Часть 2. Эта часть про обнуление оставшихся символов в строке двумерного массива после нахождения старшего в строке : ");
             Console.WriteLine(new string('-', 120));
-            int[,] twoDimentsionalArray = new int[methodsAndProperties.HeightForTwoDimensional, methodsAndProperties.WidthForTwoDimensional];
             methodsAndProperties.ToFillArrayWithRandomNumbers(twoDimentsionalArray);
             Console.WriteLine("Изначальный массив :");
             Console.WriteLine(new string('-', 120));
@@ -31,11 +34,11 @@ namespace Lesson5Task1_Arrays_
             Console.WriteLine(new string('-', 120));
             Console.WriteLine(methodsAndProperties.ShowArray(twoDimentsionalArray));
             Console.WriteLine(new string('-', 120));
+
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(new string('-', 120));
             Console.WriteLine("Часть 3. Эта часть про обнуление оставшихся символов в строке зубчатого массива после нахождения старшего в строке : ");
             Console.WriteLine(new string('-', 120));
-            int[][] jaggedArray = new int[methodsAndProperties.AmountOfSubArrays][];
             methodsAndProperties.ToFillArrayWithRandomNumbers(jaggedArray);
             Console.WriteLine("Изначальный массив :");
             Console.WriteLine(new string('-', 120));
@@ -47,6 +50,7 @@ namespace Lesson5Task1_Arrays_
             Console.WriteLine(methodsAndProperties.ShowArray(jaggedArray));
             Console.WriteLine(new string('-', 120));
             Console.ResetColor();
+
         }
     }
 }
