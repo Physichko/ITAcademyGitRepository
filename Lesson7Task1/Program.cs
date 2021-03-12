4da9d3d0045b619a3b4398722f3a981af540f341
Physichko <79219071+Physichko@users.noreply.github.com>
2021-03-13 01:14:12 +0300

    Linq

---
+++ Lesson7Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson7Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> listOfNumbers = new List<int>() { 23, 7, 324, 46,255,32 , 6 ,4, 8, 15 ,16,23,42 };
+            var result = listOfNumbers.Select(n => n.ToString()).Select(n => n.Last()).ToList();
+            Console.WriteLine(string.Join(", ",result));
+        }
+    }
+}
---
+++ Lesson7Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson7Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> listOfNumbers = new List<int>() { 1, 2, 1, 4, 2, 5, 6, 3, 3, 1 ,2,1,5,7,1,2,3};
+            var checkCount = listOfNumbers.OrderBy(n =>n).GroupBy(n => n).Select(n => new { num = n, count = n.Count()}).Select(n => n.count);
+            var result = listOfNumbers.OrderBy(n => n).Distinct().Select(n => $"{n} Встречается {checkCount.ElementAt(n-1)} раза").ToList();
+            Console.WriteLine(string.Join("\n",result));
+        }
+    }
+}
