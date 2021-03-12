7953b9cd8aa82da57e1f083bbeeba5ea2ba76ad6
Physichko <79219071+Physichko@users.noreply.github.com>
2021-03-13 01:20:54 +0300

    updt

--- Lesson7Task2/Program.cs
+++ Lesson7Task2/Program.cs
@@ -10,7 +10,7 @@ namespace Lesson7Task2
         {
             List<int> listOfNumbers = new List<int>() { 1, 2, 1, 4, 2, 5, 6, 3, 3, 1 ,2,1,5,7,1,2,3};
             var checkCount = listOfNumbers.OrderBy(n =>n).GroupBy(n => n).Select(n => new { num = n, count = n.Count()}).Select(n => n.count);
-            var result = listOfNumbers.OrderBy(n => n).Distinct().Select(n => $"{n} Встречается {checkCount.ElementAt(n-1)} раза").ToList();
+            var result = listOfNumbers.OrderBy(n => n).Distinct().Select(n => $"{n} Встречается {checkCount.ElementAt(n-1)} раз(a)").ToList();
             Console.WriteLine(string.Join("\n",result));
         }
     }
